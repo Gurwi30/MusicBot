5f31a929bcf8fdb486a2e5a954eab3294b87c157
Glitch (gamezone-m) <none>
2020-08-30 09:37:28 +0000

    🏯🏡 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,8 @@
+const Bot = require('ctk-musicbot');
+ 
+const musicbot = new Bot({
+  Token: 'NzQ5MzM3MzEzOTYzODAyNjU0.X0qgwQ.fqvsvTar3StMnpDa92ZOzyJDB6s',
+  prefix: 'gz!' //let your prefix
+});
+ 
+musicbot.start();
